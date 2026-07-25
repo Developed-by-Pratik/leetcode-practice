de5b00d692e329f5aab9d974e0830d79bae2294c
Pratik Kakade <pratikkakade.in@gmail.com>
2026-07-25 15:16:55 +0530

    Time: 5 ms (4.11%), Space: 44.2 MB (9.12%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,25 +1,32 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        int writeIndex = m + n - 1;
-        int nums1Index = m - 1;
-        int nums2Index = n - 1;
+        int index = m;
 
-        while (nums1Index >= 0 && nums2Index >= 0) {
-            if (nums1[nums1Index] < nums2[nums2Index]) {
-                nums1[writeIndex] = nums2[nums2Index];
-                nums2Index--;
-            } else {
-                nums1[writeIndex] = nums1[nums1Index];
-                nums1Index--;
-            }
-            writeIndex--;
+        for(int i = 0 ; i < n ; i++) {
+            nums1[index++] = nums2[i];
         }
+        Arrays.sort(nums1);
 
-        while (nums2Index >= 0) {
-            nums1[writeIndex] = nums2[nums2Index];
-            nums2Index--;
-            writeIndex--;
-        }
+        // int writeIndex = m + n - 1;
+        // int nums1Index = m - 1;
+        // int nums2Index = n - 1;
+
+        // while (nums1Index >= 0 && nums2Index >= 0) {
+        //     if (nums1[nums1Index] < nums2[nums2Index]) {
+        //         nums1[writeIndex] = nums2[nums2Index];
+        //         nums2Index--;
+        //     } else {
+        //         nums1[writeIndex] = nums1[nums1Index];
+        //         nums1Index--;
+        //     }
+        //     writeIndex--;
+        // }
+
+        // while (nums2Index >= 0) {
+        //     nums1[writeIndex] = nums2[nums2Index];
+        //     nums2Index--;
+        //     writeIndex--;
+        // }
     }
 }
