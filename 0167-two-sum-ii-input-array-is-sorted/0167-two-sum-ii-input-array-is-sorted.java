2fa74a3907b3abbff40b13b402a1522722ab83d6
Pratik Kakade <pratikkakade.in@gmail.com>
2026-07-27 10:08:18 +0530

    Time: 2 ms (95.56%), Space: 48.5 MB (24.28%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,16 +1,34 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        
-        for(int i = 0 ; i < numbers.length ; i++) {
-            for(int j = i+1 ; j < numbers.length ; j++) {
 
-                if(numbers[i]+numbers[j] == target) {
-                    return new int[]{i+1,j+1};
-                }
+        // Brute Force approach - O(n^2)
+        //
+        // for(int i = 0 ; i < numbers.length ; i++) {
+        //     for(int j = i+1 ; j < numbers.length ; j++) {
+        //         if(numbers[i]+numbers[j] == target) {
+        //             return new int[]{i+1,j+1};
+        //         }
+        //     }
+        // }
+        // return new int[]{};
 
+        // Optimal Approach - Two Pointers
+
+        int left = 0;
+        int right = numbers.length - 1;
+
+        for (int i = 0; i < numbers.length; i++) {
+            
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[] { left + 1, right + 1 };
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
             }
         }
-
-        return new int[]{};
+        return new int[] {};
     }
 }
