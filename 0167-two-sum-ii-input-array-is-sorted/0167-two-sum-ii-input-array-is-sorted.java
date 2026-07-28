32f1c188ab5758c71db2d77dab411bcca5403d79
Pratik Kakade <pratikkakade.in@gmail.com>
2026-07-28 23:02:15 +0530

    Time: 6 ms (12.86%), Space: 49.5 MB (5.18%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -12,23 +12,40 @@ class Solution {
         // }
         // return new int[]{};
 
+        // Better Approach 
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            int complement = target - numbers[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement) + 1, i + 1};
+            }
+
+            map.put(numbers[i], i);
+        }
+
+        return new int[]{};
+
         // Optimal Approach - Two Pointers
 
-        int left = 0;
-        int right = numbers.length - 1;
+        // int left = 0;
+        // int right = numbers.length - 1;
 
-        while(left < right) {
+        // while (left < right) {
 
-            int sum = numbers[left] + numbers[right];
+        //     int sum = numbers[left] + numbers[right];
 
-            if (sum == target) {
-                return new int[] { left + 1, right + 1 };
-            } else if (sum < target) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-        return new int[] {};
+        //     if (sum == target) {
+        //         return new int[] { left + 1, right + 1 };
+        //     } else if (sum < target) {
+        //         left++;
+        //     } else {
+        //         right--;
+        //     }
+        // }
+        // return new int[] {};
     }
 }
